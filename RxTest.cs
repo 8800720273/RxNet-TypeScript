3dda55eca56878edaebbe944aedeaa875d68d1a5
BAL KRISHNA <bk1792245@gmail.com>
2018-04-10 20:49:50 +0530

    Rx.net

---
+++ RxTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RxNet
+{
+    class RxTest<T> : IObserver<T>
+    {
+        public void OnNext(T value)
+        {
+            Console.WriteLine("Hi TeamLead");
+
+        }
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Error {0}", error);
+        }
+        public void OnCompleted()
+        {
+           
+        }
+    }
+    public class MySequenceOfNumbers : IObservable<int>
+    {
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            observer.OnNext( 'M');
+            Thread.Sleep(2000);
+            observer.OnNext('N');
+            observer.OnCompleted();
+            return Disposable.Empty;
+        }
+    }
+    public class RxMain
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new MySequenceOfNumbers();
+            var observer = new RxTest<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                numbers.Subscribe(observer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+ 
+    
---
+++ RxTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RxNet
+{
+    class RxTest1
+    {
+        static void WriteSequenceToConsole(IObservable<string> sequence)
+        {
+            //The next two lines are equivalent.
+            //sequence.Subscribe(value=>Console.WriteLine(value));
+            sequence.Subscribe(Console.WriteLine);
+        }
+        static void Main(string[] args)
+        {
+            var subject = new Subject<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                subject.OnNext("Message 1");
+                WriteSequenceToConsole(subject);
+                Thread.Sleep(2000);
+                subject.OnNext("Message 2");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
